ca1933ea779943e1940354704e230abd0c5b3279
prathameshpailwan <prathameshpailwan02@gmail.com>
2024-12-04 23:07:26 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PurchaseComponent } from './components/main/purchase/purchase.component';
+import { SaleComponent } from './components/main/sale/sale.component';
+import { ReportsComponent } from './components/main/reports/reports.component';
+import { MasterComponent } from './components/main/master/master.component';
+import { DashboardComponent } from './components/main/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'purchase', component: PurchaseComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'sale', component: SaleComponent },
+    { path: 'master', component: MasterComponent },
+    { path: 'report', component: ReportsComponent },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' } 
+];
